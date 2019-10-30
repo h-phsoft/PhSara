bf4c786ef821fe32894e7dd8544e7a94b3659c8d
h-phsoft <h.phsoft@gmail.com>
2019-10-30 11:37:23 +0200

    Merge origin/master
    Conflicts:
    	db/eadrx.sql

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,4 @@
-$(document).ready(function () {
 
-  $(".slider").slick({
-    dots: true,
-    center: true,
-    infinite: true,
-    variableWidth: true,
-    mobileFirst: true
-  });
-  $('#lightgallery').lightGallery({
-    share: false,
-    download: false,
-    counter: false,
-    scale: 0.5
-  });
 
-});
+
+
